398cad6ccb36b47d56dcfdacb5cd80f8f8f38171
relvaner <david4bauer@googlemail.com>
2021-01-29 02:06:45 +0100

    Added PseudoActorWithRx, ConcurrentPseudoActorWithRx

---
+++ src/main/java/io/actor4j/corex/actors/PseudoActorWithRx.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2015-2020, David A. Bauer. All rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.actor4j.corex.actors;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+import io.actor4j.core.ActorSystem;
+import io.actor4j.core.PseudoActorCell;
+import io.actor4j.core.actors.PseudoActor;
+import io.actor4j.core.messages.ActorMessage;
+import io.actor4j.corex.utils.ActorMessageFlowable;
+import io.reactivex.Flowable;
+
+public abstract class PseudoActorWithRx extends PseudoActor {
+	protected final Flowable<ActorMessage<?>> rxOuterQueueL1;
+	
+	public PseudoActorWithRx(ActorSystem system, boolean blocking) {
+		this(null, system, blocking);
+	}
+	
+	public PseudoActorWithRx(String name, ActorSystem system, boolean blocking) {
+		super(name, system, blocking);
+		rxOuterQueueL1 = ActorMessageFlowable.getMessages(((PseudoActorCell)getCell()).getOuterQueueL1());
+	}
+
+	public boolean run() {
+		return ((PseudoActorCell)cell).run();
+	}
+	
+	public boolean runOnce() {
+		return ((PseudoActorCell)cell).runOnce();
+	}
+	
+	public  Stream<ActorMessage<?>> runWithStream() {
+		return ((PseudoActorCell)cell).runWithStream();
+	}
+	
+	public Flowable<ActorMessage<?>> runWithRx() {
+		boolean hasNextOuter = ((PseudoActorCell)cell).getOuterQueueL1().peek()!=null;
+		if (!hasNextOuter && ((PseudoActorCell)cell).getOuterQueueL2().peek()!=null) {
+			ActorMessage<?> message = null;
+			for (int j=0; (message=((PseudoActorCell)cell).getOuterQueueL2().poll())!=null && j<((PseudoActorCell)cell).getSystem().getBufferQueueSize(); j++)
+				((PseudoActorCell)cell).getOuterQueueL1().offer(message);
+		}
+		
+		return rxOuterQueueL1;
+	}
+	
+	public ActorMessage<?> await() {
+		return ((PseudoActorCell)cell).await();
+	}
+	
+	public ActorMessage<?> await(long timeout, TimeUnit unit) throws InterruptedException, TimeoutException {
+		return ((PseudoActorCell)cell).await(timeout, unit);
+	}
+	
+	public <T> T await(Predicate<ActorMessage<?>> predicate, Function<ActorMessage<?>, T> action, long timeout, TimeUnit unit) throws InterruptedException, TimeoutException {
+		return ((PseudoActorCell)cell).await(predicate, action, timeout, unit);
+	}
+	
+	public void reset() {
+		((PseudoActorCell)cell).reset();
+	}
+}
--- src/test/java/io/actor4j/corex/features/XPseudoActorFeature.java
+++ src/test/java/io/actor4j/corex/features/XPseudoActorFeature.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2018, David A. Bauer. All rights reserved.
+ * Copyright (c) 2015-2017, David A. Bauer. All rights reserved.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,15 +15,117 @@
  */
 package io.actor4j.corex.features;
 
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Before;
+import org.junit.Test;
 
 import io.actor4j.core.ActorSystem;
-import io.actor4j.core.features.PseudoActorFeature;
+import io.actor4j.core.actors.Actor;
+import io.actor4j.core.messages.ActorMessage;
+import io.actor4j.core.utils.ActorFactory;
 import io.actor4j.corex.XActorSystemImpl;
+import io.actor4j.corex.actors.PseudoActorWithRx;
 
-public class XPseudoActorFeature extends PseudoActorFeature {
+import static io.actor4j.core.logging.user.ActorLogger.logger;
+import static org.junit.Assert.*;
+
+public class XPseudoActorFeature {
+	protected ActorSystem system;
+	
 	@Before
 	public void before() {
 		system = new ActorSystem("x-actor4j", XActorSystemImpl.class);
+	}
+	
+	@Test(timeout=5000)
+	public void test() {
+		CountDownLatch testDone = new CountDownLatch(10);
+		
+		PseudoActorWithRx main = new PseudoActorWithRx(system, false) {
+			@Override
+			public void receive(ActorMessage<?> message) {
+			}
+		};
+		
+		final int[] postconditions_numbers = new int[] { 341, 351, 451, 318, 292, 481, 240, 478, 382, 502, 158, 401, 438, 353, 165, 344, 6, 9, 18, 31, 77, 90, 45, 63, 190, 1 };
+		
+		UUID numberGenerator = system.addActor(new ActorFactory() {
+			@Override
+			public Actor create() {
+				return new Actor("numberGenerator") {
+					protected ScheduledFuture<?> timerFuture;
+					protected int counter = 0;
+					
+					@Override
+					public void preStart() {
+						timerFuture = system.timer()
+							.schedule(() -> new ActorMessage<Integer>(postconditions_numbers[counter++], 0, self(), null), main.getId(), 0, 25, TimeUnit.MILLISECONDS);
+					}
+					
+					@Override
+					public void receive(ActorMessage<?> message) {
+						logger().debug(String.format("numberGenerator received a message.tag (%d) from main", message.tag));
+						testDone.countDown();
+					}
+					
+					@Override
+					public void postStop() {
+						timerFuture.cancel(true);
+					}
+				};
+			}
+		});
+		
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+			protected int i;
+			protected int counter = 0;
+			@Override
+			public void run() {
+				main.runWithRx()
+					.take(2)
+					.forEach(msg -> { 
+						assertEquals(postconditions_numbers[counter++], msg.valueAsInt()); 
+						logger().debug("-> main received a message.value ("+msg.valueAsInt()+") from numberGenerator");
+					}); 
+					
+				main.send(new ActorMessage<>(null, i++, main.getId(), numberGenerator));
+			}
+		}, 0, 50);
+		
+		system.start();
+		
+		try {
+			testDone.await();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		timer.cancel();
+		system.shutdownWithActors(true);
+	}
+	
+	@Test(timeout=5000)
+	public void test_await() {
+		ActorSystem system = new ActorSystem();
+		
+		PseudoActorWithRx main = new PseudoActorWithRx(system, true) {
+			@Override
+			public void receive(ActorMessage<?> message) {
+			}
+		};
+		
+		system.start();
+		
+		system.send(new ActorMessage<Boolean>(true, 0, system.SYSTEM_ID, main.getId()));
+		ActorMessage<?> message1 = main.await();
+		assertEquals(true, message1.valueAsBoolean());
+
+		system.shutdownWithActors(true);
 	}
 }
